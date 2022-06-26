446a3d2d7d391853e260b0cf6358ff2593d68eda
eatsalonedev <joshuacharley87@gmail.com>
2022-06-26 19:39:11 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import './table.css';
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [data, getData] = useState([])
+    const URL = 'http://localhost:3000/mark';
 
+ 
+    useEffect(() => {
+        fetchData()
+    }, [])
+ 
+ 
+    const fetchData = () => {
+        fetch(URL)
+            .then((res) =>
+                res.json())
+                
+ 
+            .then((response) => {
+                console.log(response);
+                getData(response);
+            })
+ 
+    }
+ 
+    return (
+      
+        <>
+            <h1>Display of student marks and total</h1>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Physics</th>
+                    <th>Maths</th>
+                    <th>English</th>
+                    <th>Total</th>
+                </tr>
+                {data.map((item, i) => (
+                  item.total = item.physics + item.maths + item.english,
+                    <tr key={i}>
+                        <td>{item.name}</td>
+                        <td>{item.physics}</td>
+                        <td>{item.maths}</td>
+                        <td>{item.english}</td>
+                        <td>{item.total}</td>
+                    </tr>
+                ))}
+            </tbody>
+ 
+        </>
+    );
+}
+ 
 export default App;
